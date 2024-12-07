c9a451659d443a1909e698048c7ddc55a3f3bd4a
1i9h7_b1u3 <1012796366@qq.com>
2024-12-07 17:52:26 +0800

    README.md Generator

--- generateReadMe.py
+++ generateReadMe.py
@@ -6,17 +6,21 @@ def generateMarkdown():
     newFollowFlag = os.path.exists(config.outputNewFollowerPath)
 
     with open("./README.md", "w", encoding="utf-8") as targetFile:
-        targetFile.write("# Statistic\n")
-        targetFile.write("## Users who unfollowed you:\n")
+        with open(config.defaultREADME, "r", encoding="utf-8") as sourceFile:
+            targetFile.write(sourceFile.read())
+            targetFile.write("\n")
+
+        targetFile.write(f"# Statistic of {config.accountName} || {config.accountName} 的数据\n")
+        targetFile.write("## Users who unfollowed you || 取关列表\n")
         if unfollowFlag:
             with open(config.outputUnfollowerPath, "r", encoding="utf-8") as unfollowFile:
                 targetFile.write(unfollowFile.read())
         else:
-            targetFile.write("Nothing here!\n")
+            targetFile.write("Nothing here! || 啥也没有！\n")
 
-        targetFile.write("## Users who followed you yesterday:\n")
+        targetFile.write("## Users who followed you yesterday || 昨日新增\n")
         if newFollowFlag:
             with open(config.outputNewFollowerPath, "r", encoding="utf-8") as newFollowerFile:
                 targetFile.write(newFollowerFile.read())
         else:
-            targetFile.write("Nothing here!")
+            targetFile.write("Nothing here! || 啥也木有！\n")
